8a653051508352f6d9148da51cbd56648a699eac
Leandro Burgos <burgosleandro.c@gmail.com>
2022-04-08 23:49:18 -0400

    microservice

---
+++ src/main/java/microservice/springboot/demo01/Demo01Application.java
@@ -0,0 +1,13 @@
+package microservice.springboot.demo01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo01Application.class, args);
+	}
+
+}
